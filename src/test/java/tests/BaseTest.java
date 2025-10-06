43eb7ca36122738529e5325c8d442e2e02615285
TemnikovES <Gism092@mail.ru>
2025-10-06 17:11:56 +0300

    Первые тесты

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.*;
+import pages.HomePage;
+import pages.LoginPage;
+import utils.PropertyReader;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
+
+public class BaseTest {
+
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+
+    @BeforeMethod
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+        Configuration.baseUrl = PropertyReader.getProperty("Skyrexio.url");
+        Configuration.browserSize = "1920x1080";
+        loginPage = new LoginPage();
+        homePage = new HomePage();
+    }
+
+    @AfterMethod
+    public void close() {
+        clearBrowserCache();
+        closeWebDriver();
+    }
+}
